806b417ceaaaadd5ef318062b246a009b7a9c1d3
mfarsx <arslantrkfatih@gmail.com>
2024-02-24 19:31:47 +1100

    Add command handler for "addpayment" command

--- src/bot.js
+++ src/bot.js
@@ -1,29 +1,57 @@
 const eris = require("eris");
 require("dotenv").config({ path: "./config/.env" });
 
+const PREFIX = "pb!";
+
 const bot = new eris.Client(process.env.BOT_TOKEN);
 
 bot.on("ready", () => {
   console.log("Connected and ready.");
 });
 
-bot.on("messageCreate", async (message) => {
-  const botWasMentioned = message.mentions.find(
-    (mentionedUser) => mentionedUser.id === bot.user.id
-  );
+const commandHandlerForCommandName = {};
 
-  if (botWasMentioned) {
-    try {
-      await message.channel.createMessage("Present");
-    } catch (error) {
-      console.warn("Failed to respond to mention");
-      console.warn(error);
-    }
+commandHandlerForCommandName["addpayment"] = (message, args) => {
+  const mention = args[0];
+  const amount = parseFloat(args[1]);
+
+  return message.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`);
+};
+
+bot.on("messageCreate", async (message) => {
+  const content = message.content;
+
+  if (!message.channel.guild) {
+    return;
+  }
+  if (!content.startsWith(PREFIX)) {
+    return;
+  }
+
+  const parts = content
+    .split(" ")
+    .map((s) => s.trim())
+    .filter((s) => s);
+
+  const commandName = parts[0].substring(PREFIX.length);
+
+  const commandHandler = commandHandlerForCommandName[commandName];
+  if (!commandHandler) {
+    return;
+  }
+
+  const args = parts.slice(1);
+
+  try {
+    await commandHandler(message, args);
+  } catch (err) {
+    console.warn("Error handling command");
+    console.warn(err);
   }
 });
 
-bot.on("error", (error) => {
-  console.warn(error);
+bot.on("error", (err) => {
+  console.warn(err);
 });
 
 bot.connect();
